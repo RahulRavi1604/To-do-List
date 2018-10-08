3d50b780cfc512e083edaf2da53e75e5a29b9ac1
ubuntu <rahulravi@ideas2it.com>
2018-10-08 20:33:48 +0530

    Corrections By Trainers

--- script.js
+++ script.js
@@ -1,95 +1,187 @@
-var tasks = document.getElementsByClassName("task");
-for (var i = 0; i < tasks.length - 1; i++) {
-    tasks[i].addEventListener('click', open, false);
-}
-var open = function(evt) {
-    var desc = document.getElementsByClassName("task-description")[0];
-    var list = document.getElementsByClassName("todo")[0];
 
-    var tasktext = document.getElementsByClassName("task-text")[0];
-    tasktext.innerHTML = (evt.target.innerText);
+    var tasks = getElementsByClassName("task");
+    var desc = getElementByClassName("task-description");
+    var todo = getElementByClassName("todo");
+    var slider = getElementByClassName("toggle-slider");
+    var list = getElementByClassName("todo");
+    var tasktext = getElementByClassName("task-text");
+    var deleteButton = getElementByClassName("delete-task");
+    var tasktext = getElementByClassName("task-text");
+    var plusIcon = getElementByClassName("plus-icon");
+    var addInput = getElementByClassName("add-input");
+    var collapseButton = getElementByClassName("toggle-collapse");
+    var sidebar = getElementByClassName("sidebar");
+    var todo = getElementByClassName("todo");
+    var addButton = getElementByClassName("add-button");
+    var statusButtons = getElementsByClassName("status");
+    var importantButtons = getElementsByClassName("important");
+    
+(function () {
 
-    desc.classList.add('desc-open');
-    list.classList.add('list-open');
-    for (var i = 0; i < tasks.length; i++) {
-        tasks[i].classList.remove("active");
+    function init() {
+
+        for (var index = 0; index < tasks.length - 1; index++) {
+            tasks[index].addEventListener('click', openTaskDescriptionWindow, true);
+        }
+        slider.addEventListener('click', closeTaskDescriptionWindow, false);
+        deleteButton.addEventListener('click', deleteTask, false);
+        plusIcon.addEventListener('click', addNewTask, false);
+        collapseButton.addEventListener('click',  function() {
+            toggleClass(sidebar, "sidebar-collapse");
+            toggleClass(todo, "todo-full-width");
+       }, false);
+        addInput.addEventListener('focus', inputFocus, false);
+        addInput.addEventListener('blur', inputBlur, false);
+        tasks[tasks.length - 1].addEventListener("click", addNewTask, false);
+        addInput.addEventListener('keypress', function (e) {
+            var key = e.which || e.keyCode;
+            if (key === 13) {
+                add();
+            }
+        });
+        for (var index = 0; index < importantButtons.length; index++) {
+            importantButtons[index].addEventListener('click', toggleImportant, false);
+        }
+        for (var index = 0; index < statusButtons.length; index++) {
+            statusButtons[index].addEventListener('click',  function() {
+                toggleBetweenClasses(event.target, "fa-circle-thin", "fa-check-circle-o");
+           }, false);
+        }
     }
-    evt.target.classList.add("active");
-    evt.target.parentElement.classList.add("active");
+    init();
+}());
+
+function toggleClass(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
+function createNewElement(newElement) {
+    element = document.createElement(newElement.elementName);
+    for (var index = 0; index < newElement.classesToAdd.length; index++) {
+        element.classList.add(newElement.classesToAdd[index]);
+    }
+    if (newElement.parentElement == undefined) {
+        return element;
+    } else {
+        newElement.parentElement.appendChild(element)
+        return newElement.parentElement;
+    }
+}
+function getElementByClassName(className) {
+    return document.getElementsByClassName(className)[0];
+}
+function getElementsByClassName(className) {
+    return document.getElementsByClassName(className);
+}
+function createTaskElement(taskInput) {
+    var list = document.getElementsByClassName("list")[0];
+
+    function newElement(elementName, classesToAdd, parentNode) {
+        this.elementName = elementName,
+        this.classesToAdd = classesToAdd,
+        this.parentNode = parentNode;
+    }
+
+    newTask = createNewElement(new newElement('li',['task']));
+    taskStatus = createNewElement(new newElement('button',[], newTask));
+    taskStatusIcon = createNewElement(new newElement('i', ['fa', 'fa-circle-thin', "status"], taskStatus));
+    taskStatusIcon.addEventListener('click', toggleBetweenClasses(event.target, "fa-circle-thin", "fa-check-circle-o"), false);
+    important = createNewElement(new newElement('button',[],newTask));
+    importantIcon = createNewElement(new newElement('i', ['fa', 'fa-star-o', "important"], important));
+    importantIcon.addEventListener('click', toggleImportant, false);
+    newDiv = createNewElement(new newElement('div', ["todo-text"], newTask));
+    newText = document.createTextNode(taskInput);
+    newDiv.appendChild(newText);
+
+    
+    list.insertBefore(newTask, list.lastChild.previousSibling);
+    newTask.addEventListener('click', openTaskDescriptionWindow, false);
+    var currentListTaskCount = document.querySelector(".sidebar .active div");
+    currentListTaskCount.innerHTML = document.getElementsByClassName("task").length - 1;
+}
+
+
+
+function openTaskDescriptionWindow(evt) {
+    if (evt.target.tagName != "BUTTON" && evt.target.tagName != "I") {
+        desc.classList.add('desc-open');
+        todo.classList.add('list-open');
+        tasktext.innerHTML = (evt.target.innerText);
+        for (var index = 0; index < tasks.length; index++) {
+            tasks[index].classList.remove("active");
+        }
+    }
+    if (evt.target.classList.contains("task")) {
+        evt.target.classList.add("active");
+    } else if (evt.target.tagName == "DIV") {
+        evt.target.parentElement.classList.add("active");
+    }
 };
-
-
-
-
-var slider = document.getElementsByClassName("toggle-slider")[0];
-
-var close = function(evt) {
+function closeTaskDescriptionWindow(evt) {
     var desc = document.getElementsByClassName("task-description")[0];
-    var list = document.getElementsByClassName("todo")[0];
     desc.classList.remove('desc-open');
     list.classList.remove('list-open');
-    for (var i = 0; i < tasks.length; i++) {
-        tasks[i].classList.remove("active");
+    for (var index = 0; index < tasks.length; index++) {
+        tasks[index].classList.remove("active");
     }
 };
-slider.addEventListener('click', close, false);
-
-
-
-
-var deleteButton = document.getElementsByClassName("delete-task")[0];
-var deletefunction = function(evt) {
-    for (var i = 0; i < tasks.length; i++) {
-        if (tasks[i].classList.contains("active")) {
-            var tasktext = document.getElementsByClassName("task-text")[0];
-            if (confirm(tasktext.innerHTML+ " \nwill be deleted forever.\n You wont be able to undo this action.")) {
-               tasks[i].remove();
-               close();
+function addNewTask(evt) {
+    if (plusIcon.classList.contains("fa-plus")) {
+        plusIcon.classList.replace("fa-plus", "fa-circle-thin");
+    }
+    if (addInput.value.trim() === '' || addInput.value === "Add a Task") {
+        if (evt != undefined) {
+            if (evt.target.NodeName != 'li') {
+                document.getElementsByClassName("add-input")[0].focus();
+            }
+        }
+    } else {
+        createTaskElement(addInput.value);
+        addInput.placeholder = "Add a Task";
+        addInput.value = "";
+    }
+};
+function deleteTask(evt) {
+    for (var index = 0; index < tasks.length; index++) {
+        if (tasks[index].classList.contains("active")) {
+            if (confirm(tasktext.innerHTML + " \nwill be deleted forever.\n You wont be able to undo this action.")) {
+                tasks[index].style.height = "0%";
+                tasks[index].remove();
             }
         }
     }
 };
-deleteButton.addEventListener('click', deletefunction, false);
 
 
-var addButton = document.getElementsByClassName("plus-icon")[0];
-var addInput = document.getElementsByClassName("add-input")[0];
-var add = function(evt) {
-    addButton.classList.toggle("fa-circle-thin");
-    if (addInput.value.trim() == '') {
-        document.getElementsByClassName("add-input")[0].focus();
+function toggleBetweenClasses(element, firstClass, secondClass) {
+    if (element.classList.contains(firstClass)) {
+        element.classList.replace(firstClass, secondClass);
     } else {
-        addTask(addInput.value);
-        addInput.value = "Add a Task";
-        var tasks = document.getElementsByClassName("task");
-for (var i = 0; i < tasks.length - 1; i++) {
-    tasks[i].addEventListener('click', open, false);
-}
+        element.classList.replace(secondClass, firstClass);
     }
-};
-addButton.addEventListener('click', add, false);
-
-
-var addTask = function(taskInput) {
-    var list = document.getElementsByClassName("list")[0];
-
-    newTask = document.createElement('li');
-    newTask.classList.add("task");
-    taskStatus = document.createElement('button');
-    taskStatusIcon = document.createElement('i');
-    taskStatusIcon.classList.add("fa");
-    taskStatusIcon.classList.add("fa-circle-thin");
-    taskStatus.appendChild(taskStatusIcon);
-    important = document.createElement('button');
-    importantIcon = document.createElement('i');
-    importantIcon.classList.add("fa");
-    importantIcon.classList.add("fa-star-o");
-    important.appendChild(importantIcon);
-    newDiv = document.createElement('div');
-    newText = document.createTextNode(taskInput);
-    newDiv.appendChild(newText);
-    newTask.appendChild(taskStatus);
-    newTask.appendChild(newDiv);
-    newTask.appendChild(important);
-    list.insertBefore(newTask, list.lastChild.previousSibling);
 }
+
+
+function toggleImportant(event) {
+    if (event.target.classList.contains("fa-star-o")) {
+        event.target.classList.replace("fa-star-o", "fa-star");
+    } else {
+        event.target.classList.replace("fa-star", "fa-star-o");
+    }
+}
+function inputBlur() {
+    addButton.style.display = "none";
+    addNewTask();
+    plusIcon.classList.replace("fa-circle-thin", "fa-plus");
+}
+function inputFocus() {
+    addButton.style.display = "block";
+}
+
+
+var currentListTaskCount = document.querySelector(".sidebar .active div");
+currentListTaskCount.innerHTML = document.getElementsByClassName("task").length - 1;
+
